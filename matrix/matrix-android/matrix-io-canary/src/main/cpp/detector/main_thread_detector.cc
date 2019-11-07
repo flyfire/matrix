681c80a1719ccdfd6a8c259224ec2ae8cbd4130a
txfelixzhou <felixzhou@tencent.com>
2019-11-07 17:39:47 +0800

    bugfix: build ok

--- matrix/matrix-android/matrix-io-canary/src/main/cpp/detector/main_thread_detector.cc
+++ matrix/matrix-android/matrix-io-canary/src/main/cpp/detector/main_thread_detector.cc
@@ -30,8 +30,8 @@ namespace iocanary {
     void FileIOMainThreadDetector::Detect(const IOCanaryEnv &env, const IOInfo &file_io_info,
                                           std::vector<Issue>& issues) {
 
-        __android_log_print(ANDROID_LOG_ERROR, "FileIOMainThreadDetector", "Detect  main-thread-id：%d, thread-id:%d max_continual_rw_cost_time_μs_:%d threshold:%d"
-                , env.GetJavaMainThreadID(), file_io_info.java_context_.thread_id_, file_io_info.max_continual_rw_cost_time_μs_, env.GetMainThreadThreshold());
+        __android_log_print(ANDROID_LOG_DEBUG, "FileIOMainThreadDetector", "Detect  main-thread-id：%d, thread-id:%d max_continual_rw_cost_time_μs_:%d"
+                , GetMainThreadId(), file_io_info.java_context_.thread_id_, file_io_info.max_continual_rw_cost_time_μs_);
 
         if (GetMainThreadId() == file_io_info.java_context_.thread_id_) {
             int type = 0;
@@ -43,6 +43,7 @@ namespace iocanary {
             }
 
             if (type != 0) {
+
                 Issue issue(kType, file_io_info);
                 issue.repeat_read_cnt_ = type;  //use repeat to record type
                 PublishIssue(issue, issues);
